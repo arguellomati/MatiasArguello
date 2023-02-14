eab5678e9ca95d5b922a97cfaca4a827a2d2832f
ArguelloMati <arguellomati12@gmail.com>
2023-02-14 18:35:03 -0300

    deployment

---
+++ src/assets/main.js
@@ -0,0 +1,30 @@
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UC6PGLYu7Uc33ANAhTjEn0Qw&part=snippet%2Cid&order=date&maxResults=9"
+
+const content = null || document.getElementById("content");
+
+const options = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+		'X-RapidAPI-Key': '7d1c80615fmshc60692136162764p169689jsn5adc763f87d4',
+	}
+};
+
+async function fetchData(urlApi){const response=await fetch(urlApi,options);const data=await response.json();return data;}
+(async()=>{try{const videos=await fetchData(API);let view=`
+    ${videos.items.map(video=>`
+    <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+    <div class="group relative">
+        <div
+        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+        <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+        </h3>
+        </div>
+    </div>
+    `).slice(0,4).join('')}
+    `;content.innerHTML=view;}catch(error){console.log(error);}})();
